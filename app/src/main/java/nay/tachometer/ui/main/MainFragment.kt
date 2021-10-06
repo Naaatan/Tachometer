c001ca2d7e51fda03c98b8eb43ef056f756ac924
Yusuke Nagura <nagura@avancesys.co.jp>
2021-10-06 13:27:17 +0900

    Initial commit

---
+++ app/src/main/java/nay/tachometer/ui/main/MainFragment.kt
@@ -0,0 +1,25 @@
+package nay.tachometer.ui.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import nay.tachometer.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
---
+++ app/src/main/java/nay/tachometer/ui/main/MainViewModel.kt
@@ -0,0 +1,8 @@
+package nay.tachometer.ui.main
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
